83c6e0aac1398011c14d4a08864a4e67e8c3a11b
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-06-08 13:22:22 -0400

    added event function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 /*this will log the save task button in the console and make it easy to find */
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 var tasksToDoE1 =document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 };
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); //formE1 allows us to save varables to the formE1
